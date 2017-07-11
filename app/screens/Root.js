b01a42e9eee4dae26c61b19bfd4709b5670a7814
Teddy Koomen <email": null>
2017-07-11 10:56:27 -0400

    add dimming effect to button press

--- app/screens/Root.js
+++ app/screens/Root.js
@@ -10,23 +10,45 @@ import Login from './Login';
 class Root extends Component {
     constructor(props){
     super(props);
-
+    this.state={
+      pressedStatus: false,
+      pressedStatus2: false
+    }
       this.handleSubmit = this.handleSubmit.bind(this);
     }
   
-  handleSubmit = (routeName) => {
+  handleSubmit = (routeName) => { 
     this.props.navigation.navigate(routeName)
   }
+
+    _onHideUnderlay(){
+    this.setState({ pressStatus: false });
+  }
+  _onShowUnderlay(){
+    this.setState({ pressStatus: true });
+  }
+   _onHideUnderlay2(){
+    this.setState({ pressStatus2: false });
+  }
+  _onShowUnderlay2(){
+    this.setState({ pressStatus2: true });
+  }
+
+
 
     render() {
         return (
           <View style={styles.container}>
             <View style={styles.subcontainer}>
               <Image resizeMode="contain" style={styles.logo} source={require('../components/Images/BudgetGURU-01.png')}/>
-              <TouchableHighlight onPress={()=> this.handleSubmit('Register')} style={styles.button}>
+              <TouchableHighlight onPress={()=> this.handleSubmit('Register')} style={this.state.pressStatus2? styles.pressedButton : styles.button}
+                onHideUnderlay={this._onHideUnderlay2.bind(this)}
+                onShowUnderlay={this._onShowUnderlay2.bind(this)}>
                 <Text style={styles.buttonText}>Register</Text>
               </TouchableHighlight>
-              <TouchableHighlight onPress={()=> this.handleSubmit('Login')} style={styles.button}>
+              <TouchableHighlight onPress={()=> this.handleSubmit('Login')} style={this.state.pressStatus? styles.pressedButton : styles.button}
+                onHideUnderlay={this._onHideUnderlay.bind(this)}
+                onShowUnderlay={this._onShowUnderlay.bind(this)}>
                 <Text style={styles.buttonText}>Login</Text>
               </TouchableHighlight>
             </View>
@@ -71,6 +93,18 @@ const styles = StyleSheet.create({
     color:'#FFF',
     alignSelf:'center',
     fontSize: 18,
+
+  },
+  pressedButton:{
+    height: 50,
+    backgroundColor: '#29593c',
+    
+    alignItems: 'center',
+    marginTop: 10,
+    justifyContent: 'center',
+    borderRadius: 10,
+    width: 300,
+    alignSelf: 'center'
 
   }
 });
